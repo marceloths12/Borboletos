691e5e955ca2a3c44fa5cfa8de98274876284c84
Marcelino <marcelodzanths12@gmail.com>
2024-03-08 23:59:50 -0300

    resetting the root page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  root "home#index"
-  get 'home/index'
+  root "billets#index"
   resources :billets
   mount GoodJob::Engine, at: "good_job"
   get "up" => "rails/health#show", as: :rails_health_check
